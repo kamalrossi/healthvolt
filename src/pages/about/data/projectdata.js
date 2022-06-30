ed646b957adf3a76eed7cd799df7c7cd863d2fc4
kamalrossi <rossikamal@gmail.com>
2022-06-30 17:22:41 +0600

    first commit

---
+++ src/pages/about/data/projectdata.js
@@ -0,0 +1,56 @@
+const projectowner = "Rossi Kamal(rossikamal@gmail.com)";
+const projectname = "HealthVolt";
+const projectdesc = {
+  title: "Title",
+  desc:
+    "HealthVolt is a data science platform for healthcare domains. The interactive visual graphics help manager to see most popular doctor/department, oatient admission rate, activities of different doctors/departments and order tickets. Major modules also include billing, reportnig and inventory required in in-house clinical environment.  "
+};
+const projecttag = [
+  {
+    tag1: "ReactJS",
+    tag2: "D3.JS",
+    tag3: "CharJS",
+    tag4: "MedTech",
+    
+  }
+];
+const projecttime = [
+  {
+    title: "Software Requirement Completion",
+    from: "OCt 21",
+    to: "April 22"
+  },
+  {
+    title: "Software Development Completion",
+    from: "Oct 21",
+    to: "April 22"
+  }
+];
+
+
+const projectrole = [
+  {
+    title: "Requirement Analysis",
+    desc:
+      "Conceptualization of Data Visualization in Healthcare Domain"
+  },
+  {
+    title: "Data Science API Preparation",
+    desc:
+      " Explore and find required Data Science APIs applicable in medical domain",
+  },
+  {
+    title: "Software Development:",
+    desc:
+      "Develop and integrate Data Science modules into Admin Dashboard"
+  }
+];
+
+export {
+  projectowner,
+  projectname,
+  projectdesc,
+  projectrole,
+  projecttime,
+  projecttag
+};
